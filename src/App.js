28a39dc01887e53be46e33a5651c7bedfdb3df95
Jamie Arkin <jamielauren.arkin@gmail.com>
2020-10-05 17:37:19 -0600

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           handleChange={this.handleSearchField}
           placeholder="Search Monsters"
