610fdacecf7d4cc3c882b3152a5dcbf0b1719a2d
Jamie Arkin <jamielauren.arkin@gmail.com>
2020-10-05 17:36:48 -0600

    Updates

---
+++ precache-manifest.6ed176e9c5a2b4cc98466e9a7ca8e345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb90c9892d18956def52c494c073d71",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b9d23dda152295a37aa2",
+    "url": "/monsters-rolodex/static/css/main.11f8f30a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d23dda152295a37aa2",
+    "url": "/monsters-rolodex/static/js/main.cd10b1b3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
